1197a79c173e5b2a5b7ab3c2e91841818d7fa94c
tat22444 <tat22444@gmail.com>
2026-02-24 01:33:10 +0900

    new

---
+++ js/firebase-config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB_9nIQROt0ZyeOj9verY8WePifdB-L3_s",
+    authDomain: "ggl-login-f3407.firebaseapp.com",
+    projectId: "ggl-login-f3407",
+    storageBucket: "ggl-login-f3407.firebasestorage.app",
+    messagingSenderId: "135713948667",
+    appId: "1:135713948667:web:e7ff6937013fa387d8015e"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
